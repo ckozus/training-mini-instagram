15e8f7506209169241f3ff5b8c97c63e27979da1
Carlos Kozuszko <ckozus@gmail.com>
2015-03-18 19:55:04 -0300

    Implementando AJAX

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,13 +2,9 @@ class FollowingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    following_user = User.find(params[:user_id])
-    following = current_user.followings.build(following_user: following_user)
-    if following.save
-      redirect_to following_user
-    else
-      redirect_to user, alert: "Error al seguir!"
-    end
+    @user = User.find(params[:user_id])
+    following = current_user.followings.build(following_user: @user)
+    @success = following.save
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ActiveRecord::Base
   has_many :photos
   has_many :followings
 
-  has_many :followed_users, through: :followings, source: :following_user
+  has_many :followed_users, 
+    through: :followings, 
+    source: :following_user
 
   def following?(following_user)
     if followings.where(following_user_id: following_user.id).first
