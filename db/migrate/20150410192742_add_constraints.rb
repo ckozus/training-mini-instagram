b7ca0ea626f38507c2a1bdca01545fae6cea7390
Carlos Kozuszko <carlos@insignia4u.com>
2015-04-10 16:35:00 -0300

    Modificando migration

--- db/migrate/20150410192742_add_constraints.rb
+++ db/migrate/20150410192742_add_constraints.rb
@@ -1,6 +1,6 @@
 class AddConstraints < ActiveRecord::Migration
   def change
     add_foreign_key :followings, :users
-    add_foreign_key :followings, :following_user, column: :following_user_id
+    add_foreign_key :followings, :users, column: :following_user_id
   end
 end
